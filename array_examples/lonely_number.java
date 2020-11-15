500360954369953d249ebe4360b52c5fd7879a9d
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2020-11-15 15:21:16 +0530

    Use math to solve lonely integer problem

--- array_examples/lonely_number.java
+++ array_examples/lonely_number.java
@@ -1,11 +1,12 @@
-package array_examples;/* package codechef; // don't place package name! */
+package array_examples;
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class LonelyNumber {
+//time complexity - O(n)
+//space complexity - O(1)
+class LonelyNumberWithBitManipulation {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
---
+++ array_examples/lonely_number_using_set.java
@@ -0,0 +1,41 @@
+package array_examples;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//time complexity - O(n)
+//space complexity - O(n)
+class LonelyNumberUsingSet {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] input = new int[size];
+
+
+        for (int i = 0; i < size; i++) {
+            input[i] = sc.nextInt();
+        }
+
+        System.out.println(findLonelyNumber(input));
+    }
+
+    private static int findLonelyNumber(int[] arr) {
+        Set<Integer> set = new HashSet<Integer>();
+        int sumOfUniqueElements = 0;
+        int sumOfAllElements = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+            sumOfAllElements += arr[i];
+        }
+
+        for (int element : set) {
+            sumOfUniqueElements += element;
+        }
+
+        // 2 * (a + b + c) - (a + a + b + b + c) == c
+        return 2 * sumOfUniqueElements - sumOfAllElements;
+    }
+}
+
