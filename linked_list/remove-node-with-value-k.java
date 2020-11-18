bb8362623e5c3c75d7199bab968d63b63cc3b3cc
Pankhudi <abc@abcs-MacBook-Pro.local>
2020-11-19 00:15:03 +0530

    Remove node with given value k

---
+++ linked_list/remove-node-with-value-k.java
@@ -0,0 +1,63 @@
+package linked_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class RemoveNodesWithValueK {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(3);
+        ListNode node7 = new ListNode(3);
+        ListNode node8 = new ListNode(6);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+
+        printList(node1);
+        ListNode modifiedHead = removeNodesWithValueK(node1, 3);
+        printList(modifiedHead);
+    }
+
+    static ListNode removeNodesWithValueK(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode current = head;
+
+        while (current != null){
+            if (current.val == k) {
+                prev.next = current.next;
+            }  else {
+                prev = current;
+            }
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    private static void printList(ListNode head) {
+        while (head != null){
+            System.out.print(head.val + "-");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+}
