611cf2b3a4f4e69c6f9e6aa1d67bad89b80ba859
Pankhudi <bpankhudi7@gmail.com>
2021-01-29 01:17:51 +0530

    Check if palindrome TC -> O(n) | SC-> O(1) Correct the solution to compare between first and second half - handle case where list is odd - ignore comparing middle node with anyone

--- linked_list/IsPalindrome.java
+++ linked_list/IsPalindrome.java
@@ -5,23 +5,38 @@ class IsPalindrome {
         LinkedListHelper helper = new LinkedListHelper();
         IsPalindrome listUtility = new IsPalindrome();
 
-        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 1})));
-        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 3, 1})));
-        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2})));
-        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 3, 2, 1})));
+        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 1})) + "\n --------------------------------");
+        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 2, 1})) + "\n --------------------------------");
+        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2})) + "\n --------------------------------");
+        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{1, 2, 3, 2, 1})) + "\n --------------------------------");
+        System.out.println(listUtility.isPalindrome(helper.getListWithNum(new int[]{0, 0})) + "\n --------------------------------");
     }
 
     boolean isPalindrome(ListNode head) {
+        ListNode firstHalfLastNode = endOfFirstHalf(head);
+        Reverse listUtility = new Reverse();
+        ListNode reversedSecondHalf = listUtility.reverse(firstHalfLastNode.next);
+        LinkedListHelper helper = new LinkedListHelper();
+
+        //if list is even size - compare all
+        // if odd size ignore comparing the middle element
+        boolean result = true;
+        while (result && reversedSecondHalf != null) {
+            if (head.val != reversedSecondHalf.val) result = false;
+            head = head.next;
+            reversedSecondHalf = reversedSecondHalf.next;
+        }
+        firstHalfLastNode.next = listUtility.reverse(reversedSecondHalf);
+        return result;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode mid = slow;
-        Reverse listUtility = new Reverse();
-        ListNode reversedSecondHalf = listUtility.reverse(mid);
-        LinkedListHelper helper = new LinkedListHelper();
-        return helper.compare(head, reversedSecondHalf);
+        return slow;
     }
 }
--- linked_list/LinkedListHelper.java
+++ linked_list/LinkedListHelper.java
@@ -29,6 +29,9 @@ public class LinkedListHelper {
     }
 
     public boolean compare(ListNode l1, ListNode l2) {
+        System.out.println("Comparing : ");
+        printList(l1);
+        printList(l2);
         while (l1 != null && l2 != null) {
             if (l1.val != l2.val) return false;
             l1 = l1.next;
