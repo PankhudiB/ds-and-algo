d34c19d1cdbf2a5ada298e2e264c53ad151938df
Pankhudi <bpankhudi7@gmail.com>
2021-01-29 19:50:48 +0530

    Detect cycle in linked list - floyd tc->O(n) , sc-> O(1)

---
+++ linked_list/DetectCycle.java
@@ -0,0 +1,35 @@
+package linked_list;
+
+class DetectCycle {
+    public static void main(String[] args) {
+        DetectCycle l = new DetectCycle();
+        ListNode head = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        head.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l3;
+
+        System.out.print("has cycle : " + l.detectCycle(head) + "\n\n");
+        LinkedListHelper helper = new LinkedListHelper();
+        System.out.print("has cycle : " + l.detectCycle(helper.getListWithNum(new int[]{1, 2, 1})));
+    }
+
+    boolean detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+}
