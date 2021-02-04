0d972a988da79fb81ed17a591f7273b24af5678f
Pankhudi <bpankhudi7@gmail.com>
2021-02-04 23:39:24 +0530

    Kth Smallest Element in a BST - Bruteforce -> Inorder traversal -> sorted output -> kth element. TC -> O(n) | Sc- > O(n)

---
+++ tree/binary_search/KthSmallestElementInBST.java
@@ -0,0 +1,72 @@
+package tree.binary_search;
+
+import tree.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+class KthSmallestElementInBST {
+
+    public static void main(String[] args) {
+        Node bstHead = getBSTVariant1();
+        KthSmallestElementInBST c = new KthSmallestElementInBST();
+        System.out.println("kthSmallestElementInBST :" + c.kthSmallestElementInBST(bstHead, 3));
+
+        Node anotherBst = getBSTVariant2();
+        System.out.println("kthSmallestElementInBST :" + c.kthSmallestElementInBST(anotherBst, 1));
+    }
+
+    private static Node getBSTVariant2() {
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+//                  4
+//                /   \
+//               2     5
+//                \
+//                 3
+
+        node4.left = node2;
+        node4.right = node5;
+        node2.right = node3;
+        return node4;
+    }
+
+    private static Node getBSTVariant1() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+
+//                  5
+//                /   \
+//               3     6
+//              / \
+//             2   4
+//            /
+//           1
+        node5.left = node3;
+        node5.right = node6;
+        node3.left = node2;
+        node3.right = node4;
+        node2.left = node1;
+        return node5;
+    }
+
+    public int kthSmallestElementInBST(Node head, int k) {
+        ArrayList<Integer> inorderTraversal = new ArrayList<>();
+        inorder(head, inorderTraversal);
+        return inorderTraversal.get(k - 1);
+    }
+
+    private void inorder(Node node, ArrayList<Integer> inorderTraversal) {
+        if (node == null) return;
+        inorder(node.left, inorderTraversal);
+        inorderTraversal.add(node.val);
+        inorder(node.right, inorderTraversal);
+    }
+}
