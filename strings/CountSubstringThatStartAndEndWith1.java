67dc5ebf3fe3104d56530c76b8c7f9bc7affd0ee
Pankhudi <bpankhudi7@gmail.com>
2021-02-16 11:24:34 +0530

    Count substring that start and end with 1 - O(n) [m * (m +1) / 2 ] Number of way you can choose two 1. including duplicates. This will enable you to put anything in between and hence you get all nC2 possible substring. https://www.codechef.com/problems/CSUB

--- strings/CountSubstringThatStartAndEndWith1.java
+++ strings/CountSubstringThatStartAndEndWith1.java
@@ -12,6 +12,7 @@ class CountSubstringThatStartAndEndWith1 {
             String str = sc.nextLine();
             System.out.println(size + "-" + str);
             System.out.println(countSubstringThatStartAndEndWith1(str));
+            System.out.println(countSubstringThatStartAndEndWith1Efficient(str));
             testCases--;
         }
     }
@@ -20,12 +21,20 @@ class CountSubstringThatStartAndEndWith1 {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             for (int j = i; j < str.length(); j++) {
-                if (str.substring(i, j+1).startsWith("1") && str.substring(i, j+1).endsWith("1")) {
+                if (str.substring(i, j + 1).startsWith("1") && str.substring(i, j + 1).endsWith("1")) {
                     count++;
                 }
             }
         }
         return count;
+    }
+
+    private static int countSubstringThatStartAndEndWith1Efficient(String str) {
+        int numOf1s = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '1') numOf1s++;
+        }
+        return numOf1s * (numOf1s + 1) / 2;
     }
 }
 
