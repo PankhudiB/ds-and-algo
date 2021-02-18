5a1c37eddbf7c1b7bbd89af1b8be0b3a1cabc764
Pankhudi <bpankhudi7@gmail.com>
2021-02-19 00:25:20 +0530

    Find Maximum Path Sum - pathSumConsideringNonZeronodes while calculating MaxPathSum

--- tree/binary/MaximumPathSum.java
+++ tree/binary/MaximumPathSum.java
@@ -7,11 +7,19 @@ import java.util.*;
 class MaximumPathSum {
     int maxPathSum;
 
+    class Res {
+        public int val;
+    }
+
     public static void main(String[] args) {
         MaximumPathSum l = new MaximumPathSum();
         System.out.println("Maximum Path Sum is : " + l.maxPathSum(l.getBinaryTreeVariant1()));
         System.out.println("Maximum Path Sum is : " + l.maxPathSum(l.getBinaryTreeVariant2()));
         System.out.println("Maximum Path Sum is : " + l.maxPathSum(l.getBinaryTreeVariant3()));
+
+        System.out.println("Maximum Path Sum is : " + l.pathSumConsideringNonZeroAsMaxPathSumOuterFunc(l.getBinaryTreeVariant1()));
+        System.out.println("Maximum Path Sum is : " + l.pathSumConsideringNonZeroAsMaxPathSumOuterFunc(l.getBinaryTreeVariant2()));
+        System.out.println("Maximum Path Sum is : " + l.pathSumConsideringNonZeroAsMaxPathSumOuterFunc(l.getBinaryTreeVariant3()));
     }
 
     public int maxPathSum(Node root) {
@@ -26,6 +34,33 @@ class MaximumPathSum {
         int right = Math.max(pathSum(node.right), 0);
         maxPathSum = Math.max(maxPathSum, left + right + node.val);
         return Math.max(left, right) + node.val;
+    }
+
+//                    10
+//                 /      \
+//                 8        2
+//              /    \
+//            3       5
+
+    public int pathSumConsideringNonZeroAsMaxPathSumOuterFunc(Node node) {
+        Res res = new Res();
+        res.val = Integer.MIN_VALUE;
+        pathSumConsideringNonZeroAsMaxPathSum(node, res);
+        return res.val;
+    }
+
+    public int pathSumConsideringNonZeroAsMaxPathSum(Node node, Res res) {
+        if (node == null) return 0;
+
+        // l and r store maximum path sum going through left and
+        // right child of root respectively
+        int left = pathSumConsideringNonZeroAsMaxPathSum(node.left, res);              //leaf level -> 3 or 5
+        int right = pathSumConsideringNonZeroAsMaxPathSum(node.right, res);
+
+        int maxConsiderEitherChild = Math.max(Math.max(left, right) + node.val, node.val);  // consider either root is sufficient or 8--5 link will get select here --> 13
+        int maxConsideringRootInvolved = Math.max(maxConsiderEitherChild, left + node.val + right);   // 13 (8--5) v/s 16 (3--8--5)
+        res.val = Math.max(maxConsiderEitherChild, maxConsideringRootInvolved);
+        return maxConsiderEitherChild;      // while returning -> return maxConsiderEitherChild (13) and not 16 -- in the hope of finding bigger path upwards
     }
 
     private Node getBinaryTreeVariant1() {
