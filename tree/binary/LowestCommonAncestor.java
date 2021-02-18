7292f49245ce8fab85ddb82d4282a4ad107c68ba
Pankhudi <bpankhudi7@gmail.com>
2021-02-18 09:54:11 +0530

    Find lowest common ancestor - iterative with parent pointers

--- tree/binary/LowestCommonAncestor.java
+++ tree/binary/LowestCommonAncestor.java
@@ -2,6 +2,8 @@ package tree.binary;
 
 import tree.Node;
 
+import java.util.*;
+
 class LowestCommonAncestor {
     private Node lca;
 
@@ -19,20 +21,54 @@ class LowestCommonAncestor {
 
         System.out.println("Lowest common ancestor of 4 and 8 is :" + l.findLowestCommonAncestor(variant1Head, node4, node8));
         System.out.println("Lowest common ancestor is 3 nad 4 is :" + l.findLowestCommonAncestor(variant1Head, node4, node3));
+
+        System.out.println("Lowest common ancestor of 4 and 8 is :" + l.iterativeWithParentPointers(variant1Head, node4, node8));
+        System.out.println("Lowest common ancestor is 3 nad 4 is :" + l.iterativeWithParentPointers(variant1Head, node4, node3));
     }
 
     public Node findLowestCommonAncestor(Node head, Node p, Node q) {
-        hasDesiredNodes(head, p, q);
+        recurse(head, p, q);
         return this.lca;
     }
 
-    private boolean hasDesiredNodes(Node curr, Node p, Node q) {
+    private boolean recurse(Node curr, Node p, Node q) {
         if (curr == null) return false;
-        int left = hasDesiredNodes(curr.left, p, q) ? 1 : 0;
-        int right = hasDesiredNodes(curr.right, p, q) ? 1 : 0;
+        int left = recurse(curr.left, p, q) ? 1 : 0;
+        int right = recurse(curr.right, p, q) ? 1 : 0;
         int mid = (curr == p || curr == q) ? 1 : 0;
         if (mid + left + right >= 2) this.lca = curr;
         return (mid + left + right > 0);
+    }
+
+    private Node iterativeWithParentPointers(Node head, Node p, Node q) {
+        Stack<Node> stack = new Stack<>();
+        Map<Node, Node> parents = new HashMap<>();
+        parents.put(head, null);
+        stack.push(head);
+
+        while (!parents.containsKey(p) || !parents.containsKey(q)) {
+            Node curr = stack.pop();
+
+            if (curr.left != null) {
+                parents.put(curr.left, curr);
+                stack.push(curr.left);
+            }
+            if (curr.right != null) {
+                parents.put(curr.right, curr);
+                stack.push(curr.right);
+            }
+        }
+        Set<Node> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = parents.get(p);
+        }
+
+        while (!ancestors.contains(q)) {
+            q = parents.get(q);
+        }
+        return q;
+
     }
 
     private Node getBinaryTreeVariant1() {
