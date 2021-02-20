0ed0d3fb1497a9b4ed4590fd589cd2a8b0deae8a
Pankhudi <bpankhudi7@gmail.com>
2021-02-21 01:17:32 +0530

    Check if Balanced - bruteforce | TC-> O(n square) in case of skewed tree

---
+++ tree/binary/IsBalanced.java
@@ -0,0 +1,91 @@
+package tree.binary;
+
+import tree.Node;
+
+class IsBalanced {
+
+    public static void main(String[] args) {
+        IsBalanced l = new IsBalanced();
+        System.out.println(l.isBalanced(l.getBinaryTreeVariant1()));
+        System.out.println(l.isBalanced(l.getBinaryTreeVariant2()));
+        System.out.println(l.isBalanced(l.getCompleteBinaryTree()));
+    }
+
+    public boolean isBalanced(Node node) {
+        if (node == null) return true;
+        int lh = height(node.left);
+        int rh = height(node.right);
+        return (Math.abs(lh - rh) <= 1) && isBalanced(node.left) && isBalanced(node.right);
+    }
+
+    public int height(Node node) {
+        if (node == null) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+
+    private Node getBinaryTreeVariant1() {
+//                    10
+//                 /      \
+//                9         20
+//                        /    \
+//                      15      7
+//                        \    /
+//                         3   8
+
+        Node node7 = new Node(7);
+        Node node9 = new Node(9);
+        Node node10 = new Node(10);
+        Node node15 = new Node(15);
+        Node node20 = new Node(20);
+        Node node3 = new Node(3);
+        Node node8 = new Node(8);
+
+        node10.left = node9;
+        node10.right = node20;
+        node20.left = node15;
+        node20.right = node7;
+        node15.right = node3;
+        node7.left = node8;
+
+        return node10;
+    }
+
+    private static Node getBinaryTreeVariant2() {
+        Node node7 = new Node(7);
+        Node node9 = new Node(9);
+        Node node10 = new Node(10);
+        Node node15 = new Node(15);
+        Node node20 = new Node(20);
+
+        node10.left = node9;
+        node10.right = node20;
+        node20.left = node15;
+        node20.right = node7;
+
+        return node10;
+    }
+
+    private static Node getCompleteBinaryTree() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+//                  1
+//                /   \
+//               2     3
+//              / \   / \
+//             4   5  6   7
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        return node1;
+    }
+}
+
