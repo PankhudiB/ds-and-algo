e31538394e0f513e176c355f28d42ccfa31223f2
Pankhudi <bpankhudi7@gmail.com>
2021-02-26 21:39:26 +0530

    Refactor to extract traversals in separate classes

---
+++ graph/GraphBFS.java
@@ -0,0 +1,56 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GraphBFS {
+    private int v;
+    private ArrayList<Integer> adj[];
+
+    public GraphBFS(int v) {
+        this.v = v;
+        adj = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new ArrayList<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphBFS g = new GraphBFS(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("\nBFS");
+        g.BFS(2);
+    }
+
+    private void addEdge(int u, int v) {
+        this.adj[u].add(v);
+    }
+
+    private void BFS(int startNode) {
+        boolean[] visited = new boolean[this.v];
+        Queue<Integer> queue = new LinkedList<>();
+        visited[startNode] = true;
+        queue.add(startNode);
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            System.out.print(node + " ");
+
+            Iterator<Integer> itr = adj[node].iterator();
+            while (itr.hasNext()) {
+                Integer neighbour = itr.next();
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
+}
--- graph/GraphDFS.java
+++ graph/GraphDFS.java
@@ -2,14 +2,12 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 
-public class Graph {
+public class GraphDFS {
     private int v;
     private ArrayList<Integer> adj[];
 
-    public Graph(int v) {
+    public GraphDFS(int v) {
         this.v = v;
         adj = new ArrayList[v];
         for (int i = 0; i < v; i++) {
@@ -18,7 +16,7 @@ public class Graph {
     }
 
     public static void main(String[] args) {
-        Graph g = new Graph(5);
+        GraphDFS g = new GraphDFS(5);
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 2);
@@ -28,8 +26,6 @@ public class Graph {
 
         System.out.println("DFS");
         g.DFS(2);
-        System.out.println("\nBFS");
-        g.BFS(2);
     }
 
     private void DFS(int startNode) {
@@ -51,25 +47,5 @@ public class Graph {
 
     private void addEdge(int u, int v) {
         this.adj[u].add(v);
-    }
-
-    private void BFS(int startNode) {
-        boolean[] visited = new boolean[this.v];
-        Queue<Integer> queue = new LinkedList<>();
-        visited[startNode] = true;
-        queue.add(startNode);
-        while (!queue.isEmpty()) {
-            Integer node = queue.poll();
-            System.out.print(node + " ");
-
-            Iterator<Integer> itr = adj[node].iterator();
-            while (itr.hasNext()) {
-                Integer neighbour = itr.next();
-                if (!visited[neighbour]) {
-                    visited[neighbour] = true;
-                    queue.add(neighbour);
-                }
-            }
-        }
     }
 }
