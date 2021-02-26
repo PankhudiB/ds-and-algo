7a81069cb25b1bed1ce71b58877ccdb2ca4ff6f7
Pankhudi <bpankhudi7@gmail.com>
2021-02-27 00:09:02 +0530

    Detect Cycle in directed graph - using DFS

---
+++ graph/DetectCycle.java
@@ -0,0 +1,71 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class DetectCycle {
+    private int v;
+    private ArrayList<Integer> adj[];
+
+    public DetectCycle(int v) {
+        this.v = v;
+        adj = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new ArrayList<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        //             0
+        //          /     \
+        //         1        2
+        //                /    \
+        //              4       3
+        //                 \       \
+        //                    ---   5
+
+
+        DetectCycle graph = new DetectCycle(6);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(4, 2);
+        graph.addEdge(3, 5);
+        graph.addEdge(5, 4);
+
+        System.out.println("DFS");
+        System.out.println("isCyclic : " + graph.isCyclic(0));
+
+        DetectCycle graph2 = new DetectCycle(5);
+        graph2.addEdge(0, 1);
+        graph2.addEdge(0, 2);
+        graph2.addEdge(2, 3);
+        graph2.addEdge(2, 4);
+
+        System.out.println("isCyclic : " + graph2.isCyclic(2));
+    }
+
+    private boolean isCyclic(int startNode) {
+        boolean[] visited = new boolean[this.v];
+        return DFSUtil(startNode, visited);
+    }
+
+    private boolean DFSUtil(int curr, boolean[] visited) {
+        visited[curr] = true;
+        System.out.print(curr + " ");
+        Iterator<Integer> itr = adj[curr].iterator();
+        while (itr.hasNext()) {
+            Integer adjacentNode = itr.next();
+            if (visited[adjacentNode]) return true;
+            if (!visited[adjacentNode]) {
+                boolean isCyclic = DFSUtil(adjacentNode, visited);
+                if (isCyclic) return true;
+            }
+        }
+        return false;
+    }
+
+    private void addEdge(int u, int v) {
+        this.adj[u].add(v);
+    }
+}
