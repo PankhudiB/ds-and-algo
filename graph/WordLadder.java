97a3cc5bf29e597dcb2acd53831a05f6c9c3cf2b
Pankhudi <bpankhudi7@gmail.com>
2021-03-13 12:23:37 +0530

    Refactor WordLadder. M-> no of letters in word, N> no of words. TC -> O(Msquare*N) | SC -> O(M*N) TC of helper neighbour -> for each letter in word -> 26 possible strings and to form new string everytime it takes O(M) time, hence (26*M*M) and this neighbor will be called for all N words ,hence O(Msquare*N) SC -> although queue is use, we are removing words regularly. The Set we have initialised is size M*N (N words and each of size M)

--- graph/WordLadder.java
+++ graph/WordLadder.java
@@ -21,37 +21,44 @@ public class WordLadder {
     }
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>();
-        for (String word : wordList) {
-            wordSet.add(word);
-        }
+        Set<String> wordSet = new HashSet<>(wordList);
+
         if (!wordSet.contains(endWord)) return 0;
 
         Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
-        int level = 1;
+        int level = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String curr_word = queue.poll();
-                char[] curr_chars = curr_word.toCharArray();
-                for (int j = 0; j < curr_chars.length; j++) {          //"hit" > h , i , t
-                    char original_char = curr_chars[j];
-                    for (char c = 'a'; c < 'z'; c++) {  // for given position in [h]it --> try all a to z
-                        if (c == original_char) continue;
-                        curr_chars[j] = c;
-                        String newWord = new String(curr_chars);
-                        if (newWord.equals(endWord)) return level + 1;   //and see it has reached endWord
-                        if (wordSet.contains(newWord)) {                // or see if it is one of the transformation
-                            queue.add(newWord);
-                            wordSet.remove(newWord);
-                        }
+                if (curr_word.equals(endWord)) return level + 1;   //and see it has reached endWord
+                List<String> neighbours = neighbours(curr_word);
+                for (String neigh : neighbours) {
+                    if (wordSet.contains(neigh)) {                // or see if it is one of the transformation
+                        queue.add(neigh);
+                        wordSet.remove(neigh);
                     }
-                    curr_chars[j] = original_char;         //retain the original word
                 }
             }
             level++;
         }
         return 0;
+    }
+
+    private List<String> neighbours(String word) {
+        char[] curr_chars = word.toCharArray();
+        List<String> allPossibleNeighbours = new ArrayList<>();
+        for (int j = 0; j < curr_chars.length; j++) {          //"hit" > h , i , t
+            char original_char = curr_chars[j];
+            for (char c = 'a'; c < 'z'; c++) {                  // for given position in [h]it --> try all a to z
+                if (c == original_char) continue;
+                curr_chars[j] = c;
+                String newWord = new String(curr_chars);
+                allPossibleNeighbours.add(newWord);
+            }
+            curr_chars[j] = original_char;         //retain the original word
+        }
+        return allPossibleNeighbours;
     }
 }
