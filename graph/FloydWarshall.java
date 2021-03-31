1d2cfeda5e9fc840e4a67e600b3b426eb8337906
Pankhudi <bpankhudi7@gmail.com>
2021-04-01 00:19:32 +0530

    Floyd Warshall algo. TC -> O(V*V*V) . SC-> O(V*V) Consider each alternative path via k elements and optimize the pair path

---
+++ graph/FloydWarshall.java
@@ -0,0 +1,49 @@
+package graph;
+
+public class FloydWarshall {
+
+    private static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        int graph[][] = {{0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+        FloydWarshall d = new FloydWarshall();
+        d.allPairShortestPath(graph);
+    }
+
+    private void allPairShortestPath(int[][] graph) {
+        int V = graph.length;
+        int[][] dist = new int[V][V];
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] == INF || dist[k][j] == INF) continue;
+                    else if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        printSolution(dist);
+    }
+
+    private void printSolution(int[][] dist) {
+        for (int i = 0; i < dist.length; i++) {
+            for (int j = 0; j < dist[i].length; j++) {
+                if (dist[i][j] == INF) System.out.print("INF ");
+                else System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
