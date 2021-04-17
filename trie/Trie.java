d5961fa35c2567f121443455e30337fcdf4bc478
Pankhudi <bpankhudi7@gmail.com>
2021-04-17 12:43:10 +0530

    Implement Trie insertion and search. TC -> O(n)

---
+++ trie/Trie.java
@@ -0,0 +1,65 @@
+package trie;
+
+public class Trie {
+    private static final int ALPHABET_SIZE = 26;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            isEndOfWord = false;
+            for (int i = 0; i < ALPHABET_SIZE; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    static TrieNode root;
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        System.out.println(trie.search("searchingWhenDictionaryEmpty"));
+
+        String keys[] = {"the", "a", "there", "answer", "any",
+                "by", "bye", "their"};
+
+        for (String key : keys) {
+            trie.insert(key);
+        }
+
+        System.out.println(trie.search("the"));
+        System.out.println(trie.search("these"));
+        System.out.println(trie.search("their"));
+        System.out.println(trie.search("thaw"));
+    }
+
+    private boolean search(String key) {
+        TrieNode crawler = root;
+        for (int level = 0; level < key.length(); level++) {
+            int index = key.charAt(level) - 'a';
+            if (crawler.children[index] == null)
+                return false;
+            crawler = crawler.children[index];
+        }
+        return crawler != null && crawler.isEndOfWord;
+    }
+
+    private void insert(String key) {
+        TrieNode crawler = root;
+
+        for (int level = 0; level < key.length(); level++) {
+            int index = key.charAt(level) - 'a';
+
+            if (crawler.children[index] == null) {
+                crawler.children[index] = new TrieNode();
+            }
+            crawler = crawler.children[index];
+        }
+        crawler.isEndOfWord = true;
+    }
+}
