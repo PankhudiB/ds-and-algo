7f09a43acf5f83b58022f0116c3e95afe93c9d60
Pankhudi <bpankhudi7@gmail.com>
2021-08-19 13:05:57 +0530

    Given sorted input array including negative numbers. Result sorted squared array. O(n) | leverage the fact that the input array is sorted. and we can traverse from both ends
    We are comparing the first and last elements because after square these have the possibility of being the highest element.
    Both the extremes contain the max element (after square ofc), so we are inserting these elements to the last of the new array to make it sorted.

--- array_examples/SortedSquares.java
+++ array_examples/SortedSquares.java
@@ -6,8 +6,9 @@ class SortedSquares {
     public static void main(String[] args) throws Exception {
         int[] input = new int[]{-4, -1, 0, 3, 10};
 //        expected -> [0,1,9,16,100]
-        int[] result = sortedSquares(input);
-        Arrays.stream(result).forEach(num -> System.out.print(num + " "));
+        Arrays.stream(sortedSquares(input)).forEach(num -> System.out.print(num + " "));
+        System.out.println();
+        Arrays.stream(sortedSquaresEfficient(input)).forEach(num -> System.out.print(num + " "));
     }
 
     public static int[] sortedSquares(int[] nums) {
@@ -18,5 +19,25 @@ class SortedSquares {
         Arrays.sort(squares);
         return squares;
     }
+
+    public static int[] sortedSquaresEfficient(int[] nums) {
+        int[] result = new int[nums.length];
+        int resultIndex = nums.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            if (nums[start] * nums[start] > nums[end] * nums[end]) {
+                result[resultIndex--] = nums[start] * nums[start];
+                start++;
+            } else {
+                result[resultIndex--] = nums[end] * nums[end];
+                end--;
+            }
+        }
+
+        return result;
+    }
+
 }
 
