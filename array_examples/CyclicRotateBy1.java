76268c921510e5e8c7eb4cc2db69e44c9a39881e
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-01-16 12:45:44 +0530

    Anti-Cyclic rotate array by 1

--- array_examples/CyclicRotateBy1.java
+++ array_examples/CyclicRotateBy1.java
@@ -7,19 +7,37 @@ import java.util.List;
 
 class CyclicRotateBy1 {
     public static void main(String[] args) {
-        ArrayUtil.print(cyclicRotateBy1(new int[]{1, 2, 3, 4, 5}));
-        ArrayUtil.print(cyclicRotateBy1(new int[]{9, 8, 7, 6, 4, 2, 1, 3}));
-        ArrayUtil.print(cyclicRotateBy1(new int[]{1}));
+        int[] original = {1, 2, 3, 4, 5};
+        ArrayUtil.print(original);
+        int[] shiftedRight = cyclicRotateBy1(original);
+        ArrayUtil.print(shiftedRight);
+        ArrayUtil.print(antiCyclicRotateBy1(shiftedRight));
+        int[] ori2 = {9, 8, 7, 6, 4, 2, 1, 3};
+        ArrayUtil.print(ori2);
+        int[] arr2 = cyclicRotateBy1(ori2);
+        ArrayUtil.print(arr2);
+        ArrayUtil.print(antiCyclicRotateBy1(arr2));
     }
 
     public static int[] cyclicRotateBy1(int[] arr) {
-        int n = arr.length -1;
+        int n = arr.length - 1;
         int temp = arr[n];
         for (int i = n; i > 0; i--) {
             int desired_index = (n + (i - 1)) % n;
             arr[i] = arr[desired_index];
         }
         arr[0] = temp;
+        return arr;
+    }
+
+    public static int[] antiCyclicRotateBy1(int[] arr) {
+        int total = arr.length;
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            int desired_index = (total + (i + 1)) % total;
+            arr[i] = arr[desired_index];
+        }
+        arr[total - 1] = temp;
         return arr;
     }
 }
