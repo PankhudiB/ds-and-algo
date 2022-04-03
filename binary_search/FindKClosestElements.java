7cf2a12fd710c8a5a4b36fe4e2707bb90b782ed8
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-04-03 16:44:12 +0530

    Find K Closest Elements -> finding the left bound only takes O(log(N−k)) time from the binary search, it still costs us O(k) to build the final output. TC - O(log(N−k)+k)

--- binary_search/FindKClosestElements.java
+++ binary_search/FindKClosestElements.java
@@ -5,10 +5,18 @@ import java.util.List;
 
 public class FindKClosestElements {
     public static void main(String[] args) {
-        findKClosestElements(new int[]{1, 2, 3, 4, 5}, 4, 3);
-        findKClosestElements(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 5, 4);
-        findKClosestElements(new int[]{1}, 1, 1);
-        findKClosestElements(new int[]{1, 5, 10}, 1, 4);
+        System.out.println(findKClosestElements(new int[]{1, 2, 3, 4, 5}, 4, 3));
+        System.out.println(findKClosestElements(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 5, 4));
+        System.out.println(findKClosestElements(new int[]{1}, 1, 1));
+        System.out.println(findKClosestElements(new int[]{1, 5, 10}, 1, 4));
+        System.out.println("--------");
+
+        System.out.println(findKClosestElementsEfficient(new int[]{1, 2, 3, 4, 5}, 4, 3));
+        System.out.println(findKClosestElementsEfficient(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 5, 4));
+        System.out.println(findKClosestElementsEfficient(new int[]{1}, 1, 1));
+        System.out.println(findKClosestElementsEfficient(new int[]{1, 5, 10}, 1, 4));
+        System.out.println(findKClosestElementsEfficient(new int[]{1, 1, 2, 2, 2, 2, 2, 3, 3}, 3, 3));
+
     }
 
     public static List<Integer> findKClosestElements(int[] arr, int k, int x) {
@@ -65,4 +73,25 @@ public class FindKClosestElements {
         }
         return left;
     }
+
+    public static List<Integer> findKClosestElementsEfficient(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - k;
+        List<Integer> result = new ArrayList<>();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+
 }
