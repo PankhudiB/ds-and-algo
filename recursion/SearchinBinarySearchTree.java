604bc405c8584fc4f74a601a6bba8c3ba4055c8d
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-04-24 21:25:19 +0530

    Search in bst - recursive | tc: O(n) | sc: O(n)

---
+++ recursion/SearchinBinarySearchTree.java
@@ -0,0 +1,39 @@
+package recursion;
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        return helper(root, val);
+    }
+
+    TreeNode helper(TreeNode root, int val) {
+        if (root == null || val == root.val) return root;
+
+        if (val > root.val) {
+            return helper(root.right, val);
+        } else {
+            return helper(root.left, val);
+        }
+
+    }
+}
