19cc109e494e4a8360be7c885685455f9b70f094
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-04-27 20:25:19 +0530

    reverse string using 2 ptr

--- recursion/ReverseAString.java
+++ recursion/ReverseAString.java
@@ -17,4 +17,17 @@ public class ReverseAString {
         helper(index + 1, str);
         System.out.print(str[index] + " ");
     }
+
+    void twoPointerApproach(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
 }
