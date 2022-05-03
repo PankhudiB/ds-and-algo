0d66241f49b5c087e9236751dc155686a0a6f034
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-05-03 18:24:35 +0530

    get a row from pascal triangle - memoization + memory efficient | tc : O(k^2) | sc: O(k) --> but technically only 1 array used instead of maintianing 2 (prev and curr)

--- recursion/GetIthRowOfPascalsTriangle.java
+++ recursion/GetIthRowOfPascalsTriangle.java
@@ -19,8 +19,14 @@ public class GetIthRowOfPascalsTriangle {
         List<Integer> pascalRow7e = p.generateRowEfficient(6);
         ArrayUtil.print(pascalRow7e);
 
+        List<Integer> pascalRow4ee = p.generateRowEvenMoreEfficient(3);
+        ArrayUtil.print(pascalRow4ee);
+        List<Integer> pascalRow7ee = p.generateRowEvenMoreEfficient(6);
+        ArrayUtil.print(pascalRow7ee);
+
     }
 
+    //brute force
     public List<Integer> generateRow(int rowIndex) {
         List<Integer> ans = new ArrayList<>();
 
@@ -39,6 +45,7 @@ public class GetIthRowOfPascalsTriangle {
         return getNum(rowIndex - 1, colIndex - 1) + getNum(rowIndex - 1, colIndex);
     }
 
+    //DP
     public List<Integer> generateRowEfficient(int rowIndex) {
         List<Integer> curr = new ArrayList<>();
         List<Integer> prev = new ArrayList<>();
@@ -55,5 +62,19 @@ public class GetIthRowOfPascalsTriangle {
             prev = curr;
         }
         return prev;
+    }
+
+    //memory efficient DP
+    public List<Integer> generateRowEvenMoreEfficient(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int i = 0; i < rowIndex; i++) {
+            for (int j = i; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+            row.add(1);
+        }
+        return row;
     }
 }
