fc7ea4bf0d977b9f66666c745dae43cfec6a89da
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-05-15 12:36:24 +0530

    shortest word distance - find latest occurences of both word as you iterate - keep calculating min dist | tc : O(N*M) - n- no of words, m=length of word

--- strings/ShortestWordDistance.java
+++ strings/ShortestWordDistance.java
@@ -16,5 +16,24 @@ class ShortestWordDistance {
         }
         return minDist;
     }
+
+    public int shortestDistance2Pointer(String[] wordsDict, String word1, String word2) {
+        int minDist = wordsDict.length;
+        int word1Occurence = -1;
+        int word2Occurence = -1;
+
+        for(int i=0; i< wordsDict.length; i++){
+            if(wordsDict[i].equals(word1)) {
+                word1Occurence=i;
+            }else if(wordsDict[i].equals(word2)){
+                word2Occurence=i;
+            }
+
+            if(word1Occurence!=-1 && word2Occurence!=-1){
+                minDist = Math.min(minDist,Math.abs(word1Occurence-word2Occurence));
+            }
+        }
+        return minDist;
+    }
 }
 
