37c8d51d169039487f708c04d12a86ddc3726818
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-05-15 12:29:39 +0530

    shortest word distance - brute force | tc : O(N^2)

---
+++ strings/ShortestWordDistance.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Scanner;
+
+class ShortestWordDistance {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int minDist = wordsDict.length;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                for (int j = 0; j < wordsDict.length; j++) {
+                    if (wordsDict[j].equals(word2)) {
+                        minDist = Math.min(minDist, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDist;
+    }
+}
+
