7676fd041892983704e449b49323f1da0e46634b
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-07-10 20:46:04 +0530

    bfs template - without queue - using 2 arrays

---
+++ graph/algos/BFSWith2ArraysInsteadOfQueue.java
@@ -0,0 +1,30 @@
+package graph.algos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Algo to do BFS without queue -> rather maintain CurrentLayer & NextLayer alone
+public class BFSWith2ArraysInsteadOfQueue {
+    public void bfsWith2Arrays() {
+        List<Integer> currentLayer = new ArrayList<>();
+        List<Integer> nextLayer = new ArrayList<>();
+        int initialElement = 0; // the starting graph node
+        currentLayer.add(initialElement);
+
+        while (!currentLayer.isEmpty()) {
+            for (int cell : currentLayer) {
+                for (int neighbor : getNeighbors(cell)) {
+                    nextLayer.add(neighbor);
+                }
+            }
+
+            // Set up for processing the next layer.
+            currentLayer = nextLayer;
+            nextLayer = new ArrayList<>();
+        }
+    }
+
+    private int[] getNeighbors(int cell) {
+        return new int[]{1, 2, 3, 4};
+    }
+}
