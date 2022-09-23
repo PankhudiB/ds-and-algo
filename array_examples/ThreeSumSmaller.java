377ad094c6b599998983a6934f35811c33866634
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-09-23 23:11:35 +0530

    3SumS - sort + for each i -> 2 sum - each j -> find largest k < target - O(n^2logn)

--- array_examples/ThreeSumSmaller.java
+++ array_examples/ThreeSumSmaller.java
@@ -34,4 +34,51 @@ public class ThreeSumSmaller {
         }
         return count;
     }
+
+    // sort
+    // then for each nums[i]
+    //      apply binary search to find the largest index j such that nums[i] + nums[j] < targetnums[i]+nums[j]<target
+    //      j−i pairs that satisfy for that -> i
+    public class ThreeSumSmallerEfficient {
+        public int threeSumSmaller(int[] nums, int target) {
+            Arrays.sort(nums);
+            int sum = 0;
+            for (int i = 0; i < nums.length - 2; i++) {
+                System.out.println("main : i: " + i);
+                sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+                System.out.println("------------------------");
+            }
+            return sum;
+        }
+
+        private int twoSumSmaller(int[] nums, int startIndex, int target) {
+            System.out.println("startIndex :" + startIndex + " : " + nums[startIndex]);
+            int sum = 0;
+            for (int i = startIndex; i < nums.length - 1; i++) {
+                int j = binarySearch(nums, i, target - nums[i]);
+                System.out.println("found at: " + j + "=" + nums[j]);
+                System.out.println("i :" + i);
+                System.out.println("j-i : " + (j - i));
+                sum += j - i;
+            }
+            return sum;
+        }
+
+        private int binarySearch(int[] nums, int startIndex, int target) {
+            System.out.println("Searching :" + target + " | from :" + startIndex);
+            int left = startIndex;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                System.out.println("--------start : " + left + " | end" + right);
+                int mid = (left + right + 1) / 2;
+                if (nums[mid] < target) {
+                    left = mid;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
 }
