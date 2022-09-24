7674cbf9f42888222e5bb0e6481c13e7109c1fc8
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-09-24 10:57:24 +0530

    GroupAnagrams - create key noOfAs#noOfBs#... maintain hashmap - O(N*K) | n word, k letters

--- array_examples/GroupAnagrams.java
+++ array_examples/GroupAnagrams.java
@@ -10,6 +10,12 @@ class GroupAnagrams {
         groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
     }
 
+    // N -> no of words
+    // K -> no of letters
+
+    //for each word ->
+    // sort the string -> sorted key becomes the key of map that you collect strings against
+    // O(N*KlogK)
     public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, ArrayList<String>> map = new HashMap<>();
         for (String str : strs) {
@@ -22,6 +28,33 @@ class GroupAnagrams {
         }
 
         return new ArrayList<>(map.values());
+    }
+
+    // for each word
+    // count the alphabet count of the word -> create a key ->
+    // eg dabb --> 1#2#0#1#0#0#....
+    // use this key to collect against
+    // O(N * K)
+    public List<List<String>> groupAnagramsEfficient(String[] strs) {
+        HashMap<String,ArrayList<String>> ans = new HashMap<String,ArrayList<String>>();
+        int[] alphabetCount = new int[26];
+        for(String str : strs) {
+            Arrays.fill(alphabetCount,0);
+            char[] charStr = str.toCharArray();
+            for(int i=0; i < charStr.length; i++) {
+                alphabetCount[charStr[i] - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i < 26; i++) {
+                sb = sb.append(alphabetCount[i]);
+                sb.append("#");
+            }
+
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key,new ArrayList());
+            ans.get(key).add(str);
+        }
+        return new ArrayList(ans.values());
     }
 }
 
