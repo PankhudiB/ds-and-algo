5d546bc1d072af00d72f2a358a3fc2d1768ba349
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2022-09-27 22:20:59 +0530

    LongesstConsecutiveSequence - brute force - O(n^3)

--- array_examples/LongesstConsecutiveSequence.java
+++ array_examples/LongesstConsecutiveSequence.java
@@ -7,10 +7,15 @@ class LongesstConsecutiveSequence {
         LongesstConsecutiveSequence l = new LongesstConsecutiveSequence();
 
         System.out.println(l.longestConsecutive(new int[]{9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6}));
-
         System.out.println(l.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+        System.out.println("----------------------");
+        System.out.println(l.longestConsecutiveBruteForce(new int[]{9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6}));
+        System.out.println(l.longestConsecutiveBruteForce(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+        System.out.println("----------------------");
     }
 
+    //sort
+    // compare with previous element
     public int longestConsecutive(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
@@ -32,5 +37,38 @@ class LongesstConsecutiveSequence {
         return longestStreak;
     }
 
+    // brute force
+    // n^3 solution
+    // for each number
+    //        while loop from number to ... number+1, number+2, number+3 ...
+    //        until - u dont find the sub sequent no in input array
+
+    public int longestConsecutiveBruteForce(int[] nums) {
+        int n = nums.length;
+        int longestStreak = 0;
+        int streakSoFar = 0;
+        if (n == 0 || n == 1) return n;
+
+        for (int i = 0; i < nums.length; i++) {
+            streakSoFar = 1;
+            int current = nums[i];
+
+            while (arrayContains(nums, current + 1)) {
+                current = current + 1;
+                streakSoFar++;
+            }
+            longestStreak = Math.max(longestStreak, streakSoFar);
+        }
+        return longestStreak;
+    }
+
+    private boolean arrayContains(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
