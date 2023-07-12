55211e8701ef672d0002f4ec05f8d3511a86c120
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2023-07-12 23:04:04 +0530

    strings array problem solved using streams

---
+++ strings/AcceptHeaders.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.*;
+
+public class AcceptHeaders {
+    public static void main(String[] args) {
+        AcceptHeaders a = new AcceptHeaders();
+        System.out.println(a.parse_accept_language("en-US, fr-CA, fr-FR", new String[]{"fr-FR", "en-US"}));
+        System.out.println(a.parse_accept_language("fr-CA, fr-FR", new String[]{"en-US", "fr-FR"}));
+    }
+
+    public List<String> parse_accept_language(String clientHeader, String[] serverSupported) {
+        Set<String> serverSupportedHeaders = new HashSet<>(Arrays.asList(serverSupported));
+        List<String> result = new ArrayList<>();
+        
+        Arrays.stream(clientHeader.split(",")).map(String::trim).filter(serverSupportedHeaders::contains).forEach(result::add);
+        return result;
+    }
+}
