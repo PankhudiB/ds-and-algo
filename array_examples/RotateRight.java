aaff191b1001f261ea1bb860280071723435d713
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2023-07-15 18:22:25 +0530

    rotate right by k

--- array_examples/RotateRight.java
+++ array_examples/RotateRight.java
@@ -22,6 +22,27 @@ class RotateRight {
 
     }
 
+    public void rotateEfficient(int[] nums, int k) {
+        int n = nums.length - 1;
+        if (nums.length == 0) return;
+        k = k%(nums.length);
+
+        reverse(nums, 0,n);
+        reverse(nums, 0,k-1);
+        reverse(nums, k,n);
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
     public static int[] rotate(int[] nums, int k) {
         k %= nums.length;
         int temp, previous;
