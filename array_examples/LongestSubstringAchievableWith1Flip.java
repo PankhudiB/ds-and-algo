1e5bb44cbfb3c9deb66ccc28d4be05688e46cc66
Pankhudi Bhonsle <psbhonsle08@gmail.com>
2023-07-23 13:50:35 +0530

    sliding window to LongestSubstringAchievableWith1Flip

--- array_examples/LongestSubstringAchievableWith1Flip.java
+++ array_examples/LongestSubstringAchievableWith1Flip.java
@@ -4,34 +4,24 @@ package array_examples;
 public class LongestSubstringAchievableWith1Flip {
     public static void main(String[] args) {
         LongestSubstringAchievableWith1Flip s = new LongestSubstringAchievableWith1Flip();
-        System.out.println(s.longestSubstringAchievableWith1Flip("10011"));
+        System.out.println(s.longestSubstringAchievableWith1Flip("11001011"));
     }
 
-    //    1101101111
-//        1234511111
-// noOfZ  0010011
-//    max=50
-    private int longestSubstringAchievableWith1Flip(String s) {
-        int left = 0;
-        int currNoOfZeros = 0;
-        int ans = 0;
-        int max = 0;
+    public int longestSubstringAchievableWith1Flip(String s) {
+        int left = 0, currNoOfZeros = 0, ans = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            if (s.charAt(right) == '1') {
-                ans++;
-            } else if (s.charAt(right) == '0') {
-                currNoOfZeros++;
-                ans++;
-                if (currNoOfZeros > 1) {
-                    currNoOfZeros = 1;
-                    ans--;
-                    max = Math.max(max, ans);
-                    ans = 1;
-                }
+            if (s.charAt(right) == '0') currNoOfZeros++;
+
+            if (currNoOfZeros > 1) {
+                if (s.charAt(left) == '0')
+                    currNoOfZeros--;
+
+                left++;
             }
-            max = Math.max(max, ans);
+
+            ans = Math.max(ans, right - left + 1);
         }
-        return max;
+        return ans;
     }
 }
